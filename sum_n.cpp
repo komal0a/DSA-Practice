be4cc4248345ec4ccf9997e2caf372d585694c05
Komal <komal22124@gmail.com>
2025-06-12 15:18:50 +0530

    Added Recursion Basics

---
+++ sum_n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int print(int n, int i){
+if(n==0)return i;
+i+=n;
+return print(n-1,i);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<print(n,0)<<endl;
+    return 0;
+}
